22b6745ac87982e68d6ac0048f4b07df856bc1a6
stephany <step-quim@hotmail.com>
2017-08-12 15:47:15 -0500

    4test

--- test.py
+++ test.py
@@ -9,6 +9,26 @@ class Test(unittest.TestCase):
         PrestamodePablo = Prestamo()
         msg = PrestamodePablo.validar_prestamo(pablo)
         self.assertEquals(msg,"Prestamo aceptado")
+		#Test2 
+    def test2(self):
+        juan = Persona(1500,1600)
+        Prestamodejuan = Prestamo()
+        msg = Prestamodejuan.validar_prestamo(juan)
+        self.assertEquals(msg,"Prestamo negado")
+	
+    #Test3 
+    def test3(self):
+        ana = Persona(800,400)
+        Prestamodeana = Prestamo()
+        msg = Prestamodeana.validar_prestamo(ana)
+        self.assertEquals(msg,"Prestamo aceptado")
+	
+    #Test4 
+    def test4(self):
+        mia = Persona(800,900)
+        Prestamodemia = Prestamo()
+        msg = Prestamodemia.validar_prestamo(mia)
+        self.assertEquals(msg,"Prestamo negado")
         
 if __name__ == '__main__':
     unittest.main()
